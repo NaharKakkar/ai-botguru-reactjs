bc34f51a2ff4916e41599a99c751547240541c8c
NaharKakkar <naharkakkar1066@gmail.com>
2026-09-26 03:43:10 +0530

    Remove remaining dependency security warnings

--- client/src/Components/login/firebaseconfig.js
+++ client/src/Components/login/firebaseconfig.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "flawless-helper-403212.firebaseapp.com",
   projectId: "flawless-helper-403212",
   storageBucket: "flawless-helper-403212.appspot.com",
